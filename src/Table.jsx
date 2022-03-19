437676dbf1f83b59475ad3875fd6aa980bcf9e8f
Dants0 <gui.dg1@hotmail.com>
2022-03-18 21:13:57 -0300

    add table

---
+++ src/Table.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <table>
+        <tbody>
+            <tr>
+                <th>Name</th>
+                <th>House</th>
+                <th>Actor</th>
+            </tr>
+        </tbody>
+    </table>
+  )
+}
+
+export default Table
