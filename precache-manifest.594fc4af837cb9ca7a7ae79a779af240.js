1bac6a5c526fda84a548ee2936ea9a88ccb04ce6
Dants0 <gui.dg1@hotmail.com>
2022-04-17 13:32:43 -0300

    Updates

---
+++ precache-manifest.594fc4af837cb9ca7a7ae79a779af240.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67d9e32184ada296c9d7",
+    "url": "./static/css/main.004fd95a.chunk.css"
+  },
+  {
+    "revision": "67d9e32184ada296c9d7",
+    "url": "./static/js/main.67d9e321.chunk.js"
+  },
+  {
+    "revision": "578c1166dc35b3732d21",
+    "url": "./static/js/1.578c1166.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6e8ad98c429942fa284c090034880920",
+    "url": "./index.html"
+  }
+];
