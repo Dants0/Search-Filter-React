fbbf5836c7d61333ec6aef58b11b9dbe60a1527f
Dants0 <gui.dg1@hotmail.com>
2022-04-17 01:00:25 -0300

    correct fix

--- api/index.js
+++ api/index.js
@@ -1,30 +1,24 @@
 import express from "express";
-import cors from "cors";
-import {Users} from "./users.js";
 const app = express();
-
+import { Users } from "./users.js";
+import cors from "cors";
 
 app.use(cors());
 
-app.get('/',(req, res)=>{
+app.get("/", (req, res) => {
+  const { q } = req.query;
 
-    //var query
-    const { q } = req.query;
+  const keys = ["name","house", "patronus", "actor" ,"gender"];
 
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(q)) ||  keys.some((key) => item[key].toUpperCase().includes(q)) || keys.some((key) => item[key].includes(q))
+    );
+  };
 
-    //array de parametros
-    const keys = ["name","patronus", "{wand.length}", "house", "actor" ,"gender"];
-
-
-    //percorrendo array
-    const search = (data) => {
-        return data.filter((item) =>
-          keys.some((key) => item[key].toLowerCase().includes(q)) || keys.some((key) => item[key].toUpperCase().includes(q)) || keys.some((key) => item[key].includes(q))
-        );
-      };
-
-    q ? res.json(search(Users).slice(0,10)) : res.json(search(Users).slice(0,10));
-
+  q ? res.json(search(Users).slice(0, 20)) : res.json(Users.slice(0, 20));
 });
 
-app.listen(5000,()=>console.log('api ok'));
+app.listen(5000, () => console.log("API is working!"));
+  
+  
--- client/src/Table.jsx
+++ client/src/Table.jsx
@@ -8,7 +8,6 @@ const Table = ({data}) => {
                 <th>Name</th>
                 <th>House</th>
                 <th>Patronus</th>
-                <th>Wand</th>
                 <th>Actor</th>
                 <th>Gender</th>
                 
@@ -18,7 +17,6 @@ const Table = ({data}) => {
               <td>{item.name}</td>
               <td>{item.house}</td>
               <td>{item.patronus}</td>
-              <td>{item.wand.core}</td>
               <td>{item.actor}</td>
               <td>{item.gender}</td>
             </tr>
